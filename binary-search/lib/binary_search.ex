b0cd3362b1fb389ae8a9dbcff8d75c99a0cbbb1d
Nittin Shankar <nittin.shankar@gmail.com>
2023-05-03 09:16:57 +0530

    Finish binary search exercise

--- binary-search/lib/binary_search.ex
+++ binary-search/lib/binary_search.ex
@@ -20,42 +20,77 @@ defmodule BinarySearch do
   @spec search(tuple, integer) :: {:ok, integer} | :not_found
   def search(numbers, key) do
     numbers_list = Tuple.to_list(numbers)
+    middle_values = middle_index_and_number(numbers_list)
 
-    {middle_index, middle_number} = middle_number_and_index(numbers_list)
-
-    # Algorithm index will be 0 at the start
-    algorithm_index = 0
-
-    search(numbers_list, middle_index, middle_number, key, algorithm_index)
+    binary_search(
+      numbers_list,
+      key,
+      middle_values
+    )
   end
 
-  defp search(_numbers_list, _middle_index, middle_number, key, algorithm_index) when key == middle_number do
-    {:ok, algorithm_index}
+  def binary_search([], _key, _middle_values) do
+    :not_found
   end
 
-  defp search(numbers_list, middle_index, middle_number, key, algorithm_index) when key > middle_number do
-    count_until_middle_index = middle_index + 1
-    new_numbers_list = Enum.drop(numbers_list, count_until_middle_index)
+  def binary_search(numbers_list, key, {middle_index, middle_number}) when middle_number > key do
+    new_numbers_list = remove_right_side_of_list(numbers_list)
 
-    {middle_index, middle_number} = middle_number_and_index(new_numbers_list)
+    old_middle_index = middle_index
 
-    search(numbers_list, middle_index, middle_number, key, algorithm_index + 1)
+    {middle_index, middle_number} = middle_index_and_number(new_numbers_list)
+
+    new_middle_index =
+      if is_list_even(new_numbers_list) do
+        (old_middle_index - middle_index)
+      else
+        (old_middle_index - middle_index) - 1
+      end
+
+    new_middle_values = {new_middle_index, middle_number}
+
+    binary_search(new_numbers_list, key, new_middle_values)
   end
 
-  defp search(numbers_list, middle_index, middle_number, key, algorithm_index) when key < middle_number do
-    count_until_middle_index = (middle_index + 1) |> Integer.to_string() |> String.replace_prefix("", "-") |> String.to_integer()
-    new_numbers_list = Enum.drop(numbers_list, count_until_middle_index)
+  def binary_search(numbers_list, key, {middle_index, middle_number}) when middle_number < key do
+    new_numbers_list = remove_left_side_of_list(numbers_list)
+    old_middle_index = middle_index
 
-    {middle_index, middle_number} = middle_number_and_index(new_numbers_list)
+    {middle_index, middle_number} = middle_index_and_number(new_numbers_list)
+    new_middle_index = old_middle_index + (middle_index + 1)
 
-    search(numbers_list, middle_index, middle_number, key, algorithm_index + 1)
+    new_middle_values = {new_middle_index, middle_number}
+
+    binary_search(new_numbers_list, key, new_middle_values)
   end
 
-  defp middle_number_and_index(numbers_list) do
+  def binary_search(_numbers_list, key, {middle_index, middle_number}) when middle_number == key do
+    {:ok, middle_index}
+  end
+
+
+  def remove_left_side_of_list(numbers_list) do
+    {middle_index, _middle_number} = middle_index_and_number(numbers_list)
+    start_count = middle_index + 1
+    end_count = Enum.count(numbers_list)
+
+    Enum.slice(numbers_list, start_count..end_count)
+  end
+
+  def remove_right_side_of_list(numbers_list) do
+    {middle_index, _middle_number} = middle_index_and_number(numbers_list)
+
+    Enum.slice(numbers_list, 0, middle_index)
+  end
+
+  def middle_index_and_number(numbers_list) do
     middle_index = Enum.count(numbers_list) |> div(2)
     middle_number = Enum.at(numbers_list, middle_index)
 
     {middle_index, middle_number}
   end
 
+  def is_list_even(numbers_list) do
+    0 == length(numbers_list) |> rem(2)
+  end
 end
